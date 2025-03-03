1df2d791274fe977fcaa351f6a2ec5bbc6d84acf
Joseph Abraham <josephdevelops@gmail.com>
2025-03-02 23:28:14 -1000

    Before Submission 1_1

--- ArenaStore/src/com/storefront/cart/ShoppingCart.java
+++ ArenaStore/src/com/storefront/cart/ShoppingCart.java
@@ -11,18 +11,20 @@ public class ShoppingCart {
     public void addItem(SalableProduct product) {
         cartItems.add(product);
     }
-
+    /* when user enters '3' this is called indirectly*/
     public void removeItem(String productName) {
         cartItems.removeIf(product -> product.getName().equalsIgnoreCase(productName));
     }
 
+    /* when user enteres '4' */
     public void viewCart() {
         System.out.println("Shopping Cart:");
         for (SalableProduct item : cartItems) {
             System.out.println(item);
         }
     }
-
+    
+    /* when user enteres '5' */
     public void checkout() {
         System.out.println("Checking out the following items:");
         viewCart();
